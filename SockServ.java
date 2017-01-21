e23b61cc7b9f1b3c78219d6a15998711c60ec420
watertwo <clee10@sbcglobal.net>
2017-01-21 14:15:13 -0800

    add parse/regex
    inefficient code

--- SockServ.java
+++ SockServ.java
@@ -12,6 +12,9 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.HashMap;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.util.ArrayList;
 import java.sql.*;
 
@@ -169,7 +172,22 @@ public class SockServ {
 	 * Another way is to format using some other expression, but you'll have to tell will or keving to package it the way you want
 	 */
 	private static boolean httpParseRequest(String request) {
-		// TODO Auto-generated method stub
+      //Need cleanup, remove useless code(loop print)
+		String[] split = request.split(";");
+      int len = split.length();
+      if(len>0){
+         for(int i=0; i<len; i++){
+            Pattern p = Pattern.compile("=(.*?)");
+            Matcher m = p.matcher(split[i]);
+            if (m.find()) {
+               String check = m.group(1);
+               if(check.equals("True")==true){
+                  return true;
+               }
+            }
+         }
+      }
+        
 		return false;
 	}
 }
